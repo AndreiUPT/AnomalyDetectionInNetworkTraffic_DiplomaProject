be7fc72700c4c4b47d16d456c1b21ed5fb945e8e
andrei-r.ionescu <andrei-r.ionescu@student.upt.ro>
2024-02-06 15:44:59 +0200

    creation of html files index(home page) and result, togheter with route annotation and index function that assures connection between flask and html by render_template

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 flask_application = Flask(__name__)
+
+@flask_application.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     flask_application.run(debug=True)
