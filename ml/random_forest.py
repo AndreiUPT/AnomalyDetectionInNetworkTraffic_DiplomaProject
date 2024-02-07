0d4aec6120f5d7b94beb02afadf9c1df425e7991
andrei-r.ionescu <andrei-r.ionescu@student.upt.ro>
2024-02-07 12:18:52 +0200

    added all the necessary imports (from sklearn and pandas)

---
+++ ml/random_forest.py
@@ -0,0 +1,10 @@
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
+import socket
+import struct
+import pandas as pd
+
