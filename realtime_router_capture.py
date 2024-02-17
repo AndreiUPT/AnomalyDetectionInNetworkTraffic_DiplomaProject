7be1a190fe1dac065ac9146040e80a995b913a77
andrei-r.ionescu <andrei-r.ionescu@student.upt.ro>
2024-02-17 13:54:36 +0200

    creating a loop for packets caputuring and added a boolean variable to end the capture

--- realtime_router_capture.py
+++ realtime_router_capture.py
@@ -8,3 +8,9 @@ wifi_interface = "en0"
 
 # packets saved in a .pcap file (Live capture)
 capture = pyshark.LiveCapture(interface=wifi_interface, output_file="captured_packets.pcap")
+
+# create a loop for packets capturing
+end_capture = False  # boolean variable for ending the live capture
+for i, packet in enumerate(capture.sniff_continuously()):
+    if end_capture:
+        break
