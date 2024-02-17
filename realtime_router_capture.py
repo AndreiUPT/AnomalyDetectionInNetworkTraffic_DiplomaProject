67c4db6eb085330b90241b31445e0e99c0f2f0f5
andrei-r.ionescu <andrei-r.ionescu@student.upt.ro>
2024-02-17 13:39:30 +0200

    specifying the Wi-Fi interface and saving the live-captured packed in a .pcap file

--- realtime_router_capture.py
+++ realtime_router_capture.py
@@ -3,3 +3,8 @@
 import pyshark
 import csv
 
+# the interface for Wi-Fi capture
+wifi_interface = "en0"
+
+# packets saved in a .pcap file (Live capture)
+capture = pyshark.LiveCapture(interface=wifi_interface, output_file="captured_packets.pcap")
