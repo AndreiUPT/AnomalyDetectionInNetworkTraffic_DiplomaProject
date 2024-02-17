3527e2af2dbf3c9540cf3d5e82d1d5c02bc1fd7a
andrei-r.ionescu <andrei-r.ionescu@student.upt.ro>
2024-02-17 14:03:05 +0200

    added in an exception the code to extract relevant info from the packets

--- realtime_router_capture.py
+++ realtime_router_capture.py
@@ -14,3 +14,13 @@ end_capture = False  # boolean variable for ending the live capture
 for i, packet in enumerate(capture.sniff_continuously()):
     if end_capture:
         break
+    try:
+        # extracting the relevant info from the packet
+        time = packet.sniff_time.strftime('%Y-%m-%d %H:%M:%S')
+        source = packet.ip.src
+        destination = packet.ip.dst
+        protocol = packet.transport_layer
+        length = packet.length
+    except AttributeError:
+        pass  # skip if the packet's information is not relevant
+
