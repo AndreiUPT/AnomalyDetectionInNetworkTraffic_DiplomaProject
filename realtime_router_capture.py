f71ef4371bc62a0699b13ece37d9c305d775cf23
andrei-r.ionescu <andrei-r.ionescu@student.upt.ro>
2024-02-17 14:35:22 +0200

    creating a csv file that will be used (maybe) as a dataset for future predictions/classifications

--- realtime_router_capture.py
+++ realtime_router_capture.py
@@ -9,6 +9,11 @@ wifi_interface = "en0"
 # packets saved in a .pcap file (Live capture)
 capture = pyshark.LiveCapture(interface=wifi_interface, output_file="captured_packets.pcap")
 
+# creating a csv file for writing
+csv_file = "captured_packets.csv"
+with open(csv_file, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+
 # create a loop for packets capturing
 end_capture = False  # boolean variable for ending the live capture
 for i, packet in enumerate(capture.sniff_continuously()):
